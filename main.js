e2a33f4c324fc24a5e28c91ca358b09a78c4ed8a
Songurov <songurov@gmail.com>
2019-03-03 15:27:53 +0200

    add icon and init tray

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, Menu} = require('electron')
+const {app, BrowserWindow, Menu,Tray} = require('electron')
 const opn = require('opn');
+var path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 let author = 'https://github.com/songurov';
+let tray = null
 
 function createWindow () {
   // Create the browser window.NP
   mainWindow = new BrowserWindow({
     width: 350,
     height: 250,
-    resizable: false
+    resizable: false,
+    icon: path.join(__dirname, 'assets/hot_reload.png')
   })
+
+  tray = new Tray(path.join(__dirname, 'assets/hot_reload.png'))
+
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' }
+  ])
+  tray.setToolTip('This is my application.')
+  tray.setContextMenu(contextMenu)
 
   const name = "Reference"
   const template = [
@@ -43,45 +57,25 @@ function createWindow () {
    ]
 
   const menu = Menu.buildFromTemplate(template)
-
   Menu.setApplicationMenu(menu)
 
-  // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
